51710a87cbcf7e48ccafc25b15fc6256e7500f8e
Nikolai Markov <nikeca92@gmail.com>
2021-10-16 23:01:09 +0300

    asd

---
+++ Exam Preparation 2021/ExamPreparation2021/12.StreetRacing/Race.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StreetRacing
+{
+    public class Race
+    {
+        public List<Car> Participants { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Laps { get; set; }
+        public int Capacity { get; set; }
+        public int MaxHorsePower { get; set; }
+        public int Count
+        {
+            get { return Participants.Count; }
+        }
+
+        public Race(string name, string type, int laps, int capacity, int maxHorsePower)
+        {
+            Name = name;
+            Type = type;
+            Laps = laps;
+            Capacity = capacity;
+            MaxHorsePower = maxHorsePower;
+            Participants = new List<Car>();
+        }
+
+        public void Add(Car car)
+        {
+            if (!Participants.Any(x => x.LicensePlate == car.LicensePlate) &&
+                Capacity >= Participants.Count + 1 && car.HorsePower <= MaxHorsePower)
+            {
+                Participants.Add(car);
+            }
+        }
+
+        public bool Remove(string licensePlate)
+        {
+            var car = Participants.FirstOrDefault(x => x.LicensePlate == licensePlate);
+            if (car == null)
+            {
+                return false;
+            }
+            else
+            {
+                Participants.Remove(car);
+                return true;
+            }
+        }
+
+        public Car FindParticipant(string licensePlate)
+        {
+            return Participants.FirstOrDefault(x => x.LicensePlate == licensePlate);
+        }
+
+        public Car GetMostPowerfulCar()
+        {
+            return Participants.OrderByDescending(x => x.HorsePower).FirstOrDefault();
+
+        }
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Race: {Name} - Type: {Type} (Laps: {Laps})");
+            foreach (var participant in Participants)
+            {
+                sb.AppendLine(participant.ToString().TrimEnd());
+            }
+
+            return sb.ToString();
+        }
+    }
+}
