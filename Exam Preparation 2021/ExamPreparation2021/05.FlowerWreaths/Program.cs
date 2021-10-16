7f52c50c1255df42e222845a9a2c46bf590db532
Nikolai Markov <nikeca92@gmail.com>
2021-10-16 09:31:47 +0300

    asd

--- Exam Preparation 2021/ExamPreparation2021/05.FlowerWreaths/Program.cs
+++ Exam Preparation 2021/ExamPreparation2021/05.FlowerWreaths/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _05.FlowerWreaths
 {
@@ -6,7 +8,57 @@ namespace _05.FlowerWreaths
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack<int> lilies = new Stack<int>(Console.ReadLine()
+                .Split(", ").Select(int.Parse).ToArray());
+
+            Queue<int> roses = new Queue<int>(Console.ReadLine()
+                .Split(", ").Select(int.Parse).ToArray());
+            int wreaths = 0;
+            int flowers = 0;
+            while (roses.Count > 0 && lilies.Count > 0)
+            {
+                int lili = lilies.Pop();
+                int rose = roses.Dequeue();
+                int sum = lili + rose;
+
+                if (sum == 15)
+                {
+                    wreaths++;
+                }
+                else if (sum < 15)
+                {
+                    flowers += sum;
+                }
+                else
+                {
+                    while (true)
+                    {
+                        sum -= 2;
+                        if (sum == 15)
+                        {
+                            wreaths++;
+                            break;
+                        }
+                        else if (sum < 15)
+                        {
+                            flowers += sum;
+                            break;
+                        }
+                    }
+                }
+
+                
+            }
+            wreaths += flowers / 15;
+
+            if (wreaths >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreaths} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5 - wreaths} wreaths more!");
+            }
         }
     }
 }
