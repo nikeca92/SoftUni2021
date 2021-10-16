7e96077726e6eeb96c1aa7756845ee0a0d87e01f
Nikolai Markov <nikeca92@gmail.com>
2021-10-16 12:55:23 +0300

    ss

---
+++ Exam Preparation 2021/ExamPreparation2021/07.Bombs/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> effects = new Queue<int>(Console.ReadLine().Split(", ").Select(int.Parse));
+            Stack<int> casings = new Stack<int>(Console.ReadLine().Split(", ").Select(int.Parse));
+
+            int datura = 0;
+            int cherry = 0;
+            int smoke = 0;
+            int decrease = 0;
+            while (effects.Count> 0 && casings.Count >0)
+            {
+                if (datura >= 3 && cherry >= 3 && smoke >= 3)
+                {
+                    break;
+                }
+                int currentEffect = effects.Peek();
+                int currentCassing = casings.Peek() - decrease;
+                int combined = currentEffect + currentCassing;
+                bool bombCombined = false;
+                if (combined == 40)
+                {
+                    datura++;
+                    bombCombined = true;
+                }
+                else if (combined == 60)
+                {
+                    cherry++;
+                    bombCombined = true;
+                }
+                else if (combined == 120)
+                {
+                    smoke++;
+                    bombCombined = true;
+                }
+
+                if (bombCombined)
+                {
+                    effects.Dequeue();
+                    casings.Pop();
+                    decrease = 0;
+                }
+                else if (currentCassing <=0)
+                {
+                    casings.Pop();
+                    decrease = 0;
+                }
+                else
+                {
+                    decrease += 5;
+                }
+            }
+            if (datura >= 3 && cherry >= 3 && smoke >= 3)
+            {
+                Console.WriteLine("Bene! You have successfully filled the bomb pouch!");
+            }
+            else
+            {
+                Console.WriteLine("You don't have enough materials to fill the bomb pouch.");
+            }
+
+            if (effects.Count==0)
+            {
+                Console.WriteLine("Bomb Effects: empty");
+            }
+            else
+            {
+                Console.WriteLine($"Bomb Effects: {string.Join(", ",effects)}");
+            }
+
+            if (casings.Count==0)
+            {
+                Console.WriteLine("Bomb Casings: empty");
+            }
+            else
+            {
+                Console.WriteLine($"Bomb Casings: {string.Join(", ", casings)}");
+            }
+
+            Console.WriteLine($"Cherry Bombs: {cherry}");
+            Console.WriteLine($"Datura Bombs: {datura}");
+            Console.WriteLine($"Smoke Decoy Bombs: {smoke}");
+        }
+    }
+}
---
+++ Exam Preparation 2021/ExamPreparation2021/VetClinic/Clinic.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Channels;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private List<Pet> data;
+        public int Capacity { get; set; }
+        public int Count
+        {
+            get { return data.Count; }
+        }
+        public Clinic(int capacity)
+        {
+            data = new List<Pet>();
+            Capacity = capacity;
+        }
+
+        public void Add(Pet pet)
+        {
+            if (Capacity > data.Count)
+            {
+                data.Add(pet);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            if (data.Count > 0)
+            {
+              Pet pet = data.FirstOrDefault(x => x.Name == name);
+              if (pet == null)
+              {
+                  return false;
+              }
+              data.Remove(pet);
+              
+            }
+            return true;
+
+        }
+
+        public Pet GetPet(string name, string owner)
+        {
+            if (data.Exists(x => x.Name == name && x.Owner == owner))
+            {
+                Pet pet = data.FirstOrDefault(x => x.Name == name && x.Owner == owner);
+                return pet ;
+            }
+
+            return null;
+        }
+
+        public Pet GetOldestPet()
+        {
+            return data.OrderByDescending(x => x.Age).FirstOrDefault();
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine("The clinic has the following patients:");
+
+            foreach (var pet in data)
+            {
+                result.AppendLine($"Pet {pet.Name} with owner: {pet.Owner}");
+            }
+
+            return result.ToString();
+        }
+    }
+}
