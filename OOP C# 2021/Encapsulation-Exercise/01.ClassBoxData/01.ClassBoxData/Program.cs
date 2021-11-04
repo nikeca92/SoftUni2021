5695921674723eea45458c5ca3ebc6be16603709
Nikolai Markov <nikeca92@gmail.com>
2021-11-04 18:00:41 +0200

    100/100

---
+++ OOP C# 2021/Encapsulation-Exercise/01.ClassBoxData/01.ClassBoxData/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01.ClassBoxData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            try
+            {
+                Box box = new Box(lenght,width,height);
+                Console.WriteLine(box.GetSurfaceArea(lenght, width, height));
+                Console.WriteLine(box.GetLateralSurfaceArea(lenght, width, height));
+                Console.WriteLine(box.GetVolume(lenght, width, height));
+                
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                
+            }
+        }
+    }
+}
---
+++ OOP C# 2021/Encapsulation-Exercise/AnimalFarm/AnimalFarm/Models/Box.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace AnimalFarm.Models
+{
+    public class Box
+    {
+        private  int length;
+        private int width;
+        private int height;
+
+        public int Length
+        {
+            get { return length; }
+            private set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Length cannot be zero or negative.");
+                }
+                else
+                {
+                    length = value;
+                }
+            }
+        }
+
+        public int Width
+        {
+            get { return width; }
+            private set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Width cannot be zero or negative.");
+                }
+                else
+                {
+                    width = value;
+                }
+            }
+        }
+
+        public int Height
+        {
+            get { return height; }
+            private set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Height cannot be zero or negative.");
+                }
+                else
+                {
+                    height = value;
+
+                }
+            }
+        }
+
+        public Box(int length, int width, int height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public int GetSurfaceArea(int length,int width,int height)
+        {
+            //Surface Area = 2lw + 2lh + 2wh
+            int area = (2 * height * width) + (2 * length * height) + (2 * width * height);
+            return area;
+
+        }
+
+        public int GetLateralSurfaceArea(int length, int width, int height)
+        {
+            //Lateral Surface Area = 2lh + 2wh
+            int area = (2 * length * height) + (2 * width * height);
+            return area;
+        }
+
+        public int GetVolume(int length, int width, int height)
+        {
+            //Volume = lwh
+            int volume = length * width * height;
+            return volume;
+        }
+
+    }
+}
